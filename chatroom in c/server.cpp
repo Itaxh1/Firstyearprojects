6e9e9613320c30e091195014d410526234b19ae7
ASHWIN KUMAR <meetashwin2000@gmail.com>
2019-04-18 23:30:05 +0530

    Update server.cpp

--- chatroom in c/server.cpp
+++ chatroom in c/server.cpp
@@ -1,112 +1,195 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <string>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/uio.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <fstream>
-using namespace std;
-//Server side
-int main(int argc, char *argv[])
-{
-    //for the server, we only need to specify a port number
-    if(argc != 2)
-    {
-        cerr << "Usage: port" << endl;
-        exit(0);
-    }
-    //grab the port number
-    int port = atoi(argv[1]);
-    //buffer to send and receive messages with
-    char msg[1500];
-     
-    //setup a socket and connection tools
-    sockaddr_in servAddr;
-    bzero((char*)&servAddr, sizeof(servAddr));
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(port);
+#include <thread>
+#include <vector>
  
-    //open stream oriented socket with internet address
-    //also keep track of the socket descriptor
-    int serverSd = socket(AF_INET, SOCK_STREAM, 0);
-    if(serverSd < 0)
+#pragma comment (lib, "Ws2_32.lib")
+ 
+#define IP_ADDRESS "192.168.56.1"
+#define DEFAULT_PORT "3504"
+#define DEFAULT_BUFLEN 512
+ 
+struct client_type
+{
+    int id;
+    SOCKET socket;
+};
+ 
+const char OPTION_VALUE = 1;
+const int MAX_CLIENTS = 5;
+ 
+//Function Prototypes
+int process_client(client_type &new_client, std::vector<client_type> &client_array, std::thread &thread);
+int main();
+ 
+int process_client(client_type &new_client, std::vector<client_type> &client_array, std::thread &thread)
+{
+    std::string msg = "";
+    char tempmsg[DEFAULT_BUFLEN] = "";
+ 
+    //Session
+    while (1)
     {
-        cerr << "Error establishing the server socket" << endl;
-        exit(0);
-    }
-    //bind the socket to its local address
-    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, 
-        sizeof(servAddr));
-    if(bindStatus < 0)
-    {
-        cerr << "Error binding socket to local address" << endl;
-        exit(0);
-    }
-    cout << "Waiting for a client to connect..." << endl;
-    //listen for up to 5 requests at a time
-    listen(serverSd, 5);
-    //receive a request from client using accept
-    //we need a new address to connect with the client
-    sockaddr_in newSockAddr;
-    socklen_t newSockAddrSize = sizeof(newSockAddr);
-    //accept, create a new socket descriptor to 
-    //handle the new connection with client
-    int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
-    if(newSd < 0)
-    {
-        cerr << "Error accepting request from client!" << endl;
-        exit(1);
-    }
-    cout << "Connected with client!" << endl;
-    //lets keep track of the session time
-    struct timeval start1, end1;
-    gettimeofday(&start1, NULL);
-    //also keep track of the amount of data sent as well
-    int bytesRead, bytesWritten = 0;
-    while(1)
-    {
-        //receive a message from the client (listen)
-        cout << "Awaiting client response..." << endl;
-        memset(&msg, 0, sizeof(msg));//clear the buffer
-        bytesRead += recv(newSd, (char*)&msg, sizeof(msg), 0);
-        if(!strcmp(msg, "exit"))
+        memset(tempmsg, 0, DEFAULT_BUFLEN);
+ 
+        if (new_client.socket != 0)
         {
-            cout << "Client has quit the session" << endl;
-            break;
+            int iResult = recv(new_client.socket, tempmsg, DEFAULT_BUFLEN, 0);
+ 
+            if (iResult != SOCKET_ERROR)
+            {
+                if (strcmp("", tempmsg))
+                    msg = "Client #" + std::to_string(new_client.id) + ": " + tempmsg;
+ 
+                std::cout << msg.c_str() << std::endl;
+ 
+                //Broadcast that message to the other clients
+                for (int i = 0; i < MAX_CLIENTS; i++)
+                {
+                    if (client_array[i].socket != INVALID_SOCKET)
+                        if (new_client.id != i)
+                            iResult = send(client_array[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+                }
+            }
+            else
+            {
+                msg = "Client #" + std::to_string(new_client.id) + " Disconnected";
+ 
+                std::cout << msg << std::endl;
+ 
+                closesocket(new_client.socket);
+                closesocket(client_array[new_client.id].socket);
+                client_array[new_client.id].socket = INVALID_SOCKET;
+ 
+                //Broadcast the disconnection message to the other clients
+                for (int i = 0; i < MAX_CLIENTS; i++)
+                {
+                    if (client_array[i].socket != INVALID_SOCKET)
+                        iResult = send(client_array[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+                }
+ 
+                break;
+            }
         }
-        cout << "Client: " << msg << endl;
-        cout << ">";
-        string data;
-        getline(cin, data);
-        memset(&msg, 0, sizeof(msg)); //clear the buffer
-        strcpy(msg, data.c_str());
-        if(data == "exit")
-        {
-            //send to the client that server has closed the connection
-            send(newSd, (char*)&msg, strlen(msg), 0);
-            break;
-        }
-        //send the message to client
-        bytesWritten += send(newSd, (char*)&msg, strlen(msg), 0);
+    } //end while
+ 
+    thread.detach();
+ 
+    return 0;
+}
+ 
+int main()
+{
+    WSADATA wsaData;
+    struct addrinfo hints;
+    struct addrinfo *server = NULL;
+    SOCKET server_socket = INVALID_SOCKET;
+    std::string msg = "";
+    std::vector<client_type> client(MAX_CLIENTS);
+    int num_clients = 0;
+    int temp_id = -1;
+    std::thread my_thread[MAX_CLIENTS];
+ 
+    //Initialize Winsock
+    std::cout << "Intializing Winsock..." << std::endl;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+ 
+    //Setup hints
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+ 
+    //Setup Server
+    std::cout << "Setting up server..." << std::endl;
+    getaddrinfo(static_cast<LPCTSTR>(IP_ADDRESS), DEFAULT_PORT, &hints, &server);
+ 
+    //Create a listening socket for connecting to server
+    std::cout << "Creating server socket..." << std::endl;
+    server_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+ 
+    //Setup socket options
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &OPTION_VALUE, sizeof(int)); //Make it possible to re-bind to a port that was used within the last 2 minutes
+    setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, &OPTION_VALUE, sizeof(int)); //Used for interactive programs
+ 
+    //Assign an address to the server socket.
+    std::cout << "Binding socket..." << std::endl;
+    bind(server_socket, server->ai_addr, (int)server->ai_addrlen);
+ 
+    //Listen for incoming connections.
+    std::cout << "Listening..." << std::endl;
+    listen(server_socket, SOMAXCONN);
+ 
+    //Initialize the client list
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        client[i] = { -1, INVALID_SOCKET };
     }
-    //we need to close the socket descriptors after we're all done
-    gettimeofday(&end1, NULL);
-    close(newSd);
-    close(serverSd);
-    cout << "********Session********" << endl;
-    cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << endl;
-    cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec) 
-        << " secs" << endl;
-    cout << "Connection closed..." << endl;
-    return 0;   
+ 
+    while (1)
+    {
+ 
+        SOCKET incoming = INVALID_SOCKET;
+        incoming = accept(server_socket, NULL, NULL);
+ 
+        if (incoming == INVALID_SOCKET) continue;
+ 
+        //Reset the number of clients
+        num_clients = -1;
+ 
+        //Create a temporary id for the next client
+        temp_id = -1;
+        for (int i = 0; i < MAX_CLIENTS; i++)
+        {
+            if (client[i].socket == INVALID_SOCKET && temp_id == -1)
+            {
+                client[i].socket = incoming;
+                client[i].id = i;
+                temp_id = i;
+            }
+ 
+            if (client[i].socket != INVALID_SOCKET)
+                num_clients++;
+ 
+            //std::cout << client[i].socket << std::endl;
+        }
+ 
+        if (temp_id != -1)
+        {
+            //Send the id to that client
+            std::cout << "Client #" << client[temp_id].id << " Accepted" << std::endl;
+            msg = std::to_string(client[temp_id].id);
+            send(client[temp_id].socket, msg.c_str(), strlen(msg.c_str()), 0);
+ 
+            //Create a thread process for that client
+            my_thread[temp_id] = std::thread(process_client, std::ref(client[temp_id]), std::ref(client), std::ref(my_thread[temp_id]));
+        }
+        else
+        {
+            msg = "Server is full";
+            send(incoming, msg.c_str(), strlen(msg.c_str()), 0);
+            std::cout << msg << std::endl;
+        }
+    } //end while
+ 
+ 
+    //Close listening socket
+    closesocket(server_socket);
+ 
+    //Close client socket
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        my_thread[i].detach();
+        closesocket(client[i].socket);
+    }
+ 
+    //Clean up Winsock
+    WSACleanup();
+    std::cout << "Program has ended successfully" << std::endl;
+ 
+    system("pause");
+    return 0;
 }
